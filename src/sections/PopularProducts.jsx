16512efb42c7f7583ac5da543ffbeb542e79a8d4
Abdalrahman <123552950+AB-DALRAHM-AN@users.noreply.github.com>
2023-11-27 10:39:25 +0200

    edit in prod pop sec

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,20 +1,31 @@
 import { products } from '../constants'
+import star from '../assets/icons/star.svg'
 
 
 const PopularProducts = () => {
   return (
     <section id='products' className='max-container max-sm:mt-12'>
       <div className='flex flex-col justify-start'>
-        <div className=''>
+        <div>
           <h2 className='font-bold text-3xl'>Our <span className='text-coral-red'>Popular</span> Products</h2>
-          <p className='text-sm font-montserrat'>Experience top-notch quality and style with our sought-after
+          <p className='text-sm font-montserrat text-slate-gray'>Experience top-notch quality and style with our sought-after
           selections. Discover a world of comfort, design, and value</p>
           <div className='flex flex-row flex-wrap gap-10 flex-1 justify-center items-center'>
             {products.map((product) => (
               <div key={product.id} className='flex justify-center flex-col card items-center mt-16'>
                 <img src={product.imgURL} alt={product.name} className='object-contain mb-4' />
-                <h3 className='font-bold text-2xl'>{product.name}</h3>
-                <p className='text-xl font-montserrat text-coral-red'>{product.price}</p>
+                <span className='flex max-sm:w-full w-full flex-1 ml-2 mb-2'>
+                  <img
+                    src={star}
+                    alt='star'
+                    className='mr-2'
+                    width={20}
+                    height={20}
+                  />
+                  <span className='text-sm font-montserrat text-slate-gray'>(4.5)</span>
+                </span>
+                <h3 className='font-bold text-2xl flex flex-1 w-full ml-2 mb-2'>{product.name}</h3>
+                <p className='flex flex-1 w-full ml-2 text-xl font-montserrat text-coral-red'>{product.price}</p>
               </div>
             ))}
           </div>
